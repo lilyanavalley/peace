a9695892e2158884f9db6acc811f1545e8f94610
Lily Ana Valley <mslilyvalley@icloud.com>
2025-03-25 04:36:47 +0000

    adjust footer phosphor icon and style

--- src/components/footer.rs
+++ src/components/footer.rs
@@ -1,28 +1,29 @@
 
 use leptos::prelude::*;
+use phosphor_leptos::*;
 
 
 #[component]
 pub fn Footer() -> impl IntoView {
   view! {
-    <div class="text-center text-[#AAA] bg-[var(--color-deeperblack)]">
+    <div class="text-center text-[#AAA]">
       <p class="text-xs m-1">the website of</p>
       <p class="text-xs m-3 mb-1">Lily Ana Valley</p>
       <p class="text-xs m-1 ">
         <a href="https://github.com/lilyanavalley/peace" class="text-[var(--color-selectables-pink)]">
-          <i class="ph-fill ph-github-logo"/>
+          <div style="translateY(.2rem)"><Icon icon=GITHUB_LOGO/></div>
           Source
         </a>
-        <a class="text-[#FFF]" disabled>
-          <i class="ph-fill ph-asterisk"/>
+        <a href="https://github.com/lilyanavalley/peace/issues" class="text-[#FFF]">
+          <div style="translateY(.2rem)"><Icon icon=ASTERISK/></div>
           Issues
         </a>
         <a href="https://www.gnu.org/licenses/agpl-3.0.en.html" class="text-[var(--color-selectables-blue)]">
-          <i class="ph-fill ph-scroll"/>
+          <div style="translateY(.2rem)"><Icon icon=SCROLL/></div>
           License
         </a>
       </p>
-      <img src="assets/agplv3-155x51.png" alt="GNU AGPL version 3 License Logo"/>
+      <img src="assets/agplv3-155x51.png" alt="GNU AGPL version 3 License Logo" style="margin: 2rem"/>
     </div>
   }
 }
