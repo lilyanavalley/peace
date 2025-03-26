3b6a71419d11887c7ddfb3308c4a61dc58f1e32f
Lily Ana Valley <mslilyvalley@icloud.com>
2025-03-26 18:30:28 +0000

    Added a rainbow effect to WIP page

--- src/app.rs
+++ src/app.rs
@@ -143,7 +143,7 @@ fn NotFound() -> impl IntoView {
 #[component]
 fn Wip() -> impl IntoView {
   view! {
-    <div class="flex flex-col grow h-full overflow-auto justify-center items-center">
+    <div class="flex flex-col grow h-full overflow-auto justify-center items-center animate-(--animation-rainbowswirl)">
       <Icon icon=FLOWER size="3rem" weight=IconWeight::Duotone/>
       <p class="victor-mono-400 italic">content coming soon</p>
     </div>
