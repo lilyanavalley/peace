3f670510dc19ac4aa9d9a0b24badf6284e73a388
Lily Ana Valley <mslilyvalley@icloud.com>
2025-05-04 00:22:11 -0400

    footer uses crate name instead of hardcoded value

--- src/components/footer.rs
+++ src/components/footer.rs
@@ -14,7 +14,7 @@ pub fn Footer() -> impl IntoView {
       <p class="text-xs" style="margin-bottom: .2rem">{ placeholders::FOOTER }</p>
       <p class="text-xs" style="margin: .2rem">{ placeholders::FOOTER_NAME }</p>
       <p class="text-xs" style="margin: 1rem">
-        { format!("peacelily v{}", env!("CARGO_PKG_VERSION")) }
+        { format!("{} v{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION")) }
       </p>
       <p class="text-xs" style="margin: .2rem">
         <a href="https://github.com/lilyanavalley/peace" class="text-[var(--color-selectables-pink)]">
