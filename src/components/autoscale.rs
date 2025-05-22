e5b7b465dd7688cd61c12b2198970109fab494d6
Lily Ana Valley <mslilyvalley@icloud.com>
2025-05-22 16:20:56 -0400

    autoscale modified to grow in height

--- src/components/autoscale.rs
+++ src/components/autoscale.rs
@@ -4,7 +4,7 @@ use leptos::prelude::*;
 #[component]
 pub fn Autoscale(children: ChildrenFn) -> impl IntoView {
   view! {
-    <div id="autoscale" class="flex flex-col w-full min-h-max max-w-7xl md:w-3/4 self-center my-8">
+    <div id="autoscale" class="flex flex-col w-full max-w-7xl md:w-3/4 self-center my-8">
       { children() }
     </div>
   }
