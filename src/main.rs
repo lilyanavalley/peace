c19740eee1b163dc09f6c8d9ae4e276e5bedea13
Lily Ana Valley <mslilyvalley@icloud.com>
2025-08-15 21:42:42 +0000

    Style tweaks to colors, text size and globals

--- src/main.rs
+++ src/main.rs
@@ -68,7 +68,7 @@ async fn main() -> std::io::Result<()> {
         move || {
           view! {
             <!DOCTYPE html>
-            <html lang="en" data-theme="midnight">
+            <html lang="en" data-theme="synthetic">
               <head>
                 <meta charset="utf-8"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1,viewport-fit=cover"/>
