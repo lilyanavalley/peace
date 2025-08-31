093e339968460c6db6ce2a0e4718383329e9509c
Lily Ana Valley <mslilyvalley@icloud.com>
2025-08-31 19:14:53 -0400

    Add IndieAuth with GitHub connection

--- src/app.rs
+++ src/app.rs
@@ -23,6 +23,8 @@ pub fn App() -> impl IntoView {
     <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin=""/>
     <Link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&family=Fira+Sans:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&family=Flow+Circular&family=Victor+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet"/>
     <Link rel="shortcut icon" type_="image/ico" href="/favicon.ico"/>
+    <Link rel="authorization_endpoint" href="https://indieauth.com/auth"/>
+    <Link rel="me" href="https://github.com/lilyanavalley"/>
 
     // sets the document title
     <Title text="Lily's Website ⚛️"/>
@@ -61,6 +63,8 @@ pub fn App() -> impl IntoView {
     <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin=""/>
     <Link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&family=Fira+Sans:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&family=Flow+Circular&family=Victor+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet"/>
     <Link rel="shortcut icon" type_="image/svg+xml" href="/assets/favicon.svg"/>
+    <Link rel="authorization_endpoint" href="https://indieauth.com/auth"/>
+    <Link rel="me" href="https://github.com/lilyanavalley"/>
 
     // sets the document title
     <Title text="lily valley"/>
